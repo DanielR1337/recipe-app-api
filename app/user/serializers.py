d492be0c4d105ddc2f4758570508f732460ec677
Daniel Rendon <danielrendon93@gmail.com>
2024-10-10 15:13:54 -0600

    Adding recipe app, tests and api logic

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -21,10 +21,22 @@ class UserSerializer(serializers.ModelSerializer):
         # Create and return a user with encrypted password.
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        # Updated and return user.
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
 class AuthTokenSerializer(serializers.Serializer):
     # Serializer for the user auth token.
-    email = serializer.EmailField()
-    password = serializer.CharField(
+    email = serializers.EmailField()
+    password = serializers.CharField(
         style={'input_type': 'password'},
         trim_whitespace=False,
     )
@@ -42,5 +54,5 @@ class AuthTokenSerializer(serializers.Serializer):
             msg = _('Unable to authenticate with provided credentials.')
             raise serializers.ValidationError(msg, code='authorization')
 
-        attrs['users'] = user
+        attrs['user'] = user
         return attrs
